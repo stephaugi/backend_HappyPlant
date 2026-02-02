8431129186b28d386cc2994cc013d59c5ed71b9e
Stephanie Lin <slin.animation@gmail.com>
2026-02-01 20:43:43 -0500

    logs moisture and water. Uses data to calculate next water date and sets current moisture level

--- app/routes/plant_routes.py
+++ app/routes/plant_routes.py
@@ -134,9 +134,9 @@ def create_moisture_logs(plant_id):
         moisture_logs = plant.moisture_history
         plant.current_moisture_level = moisture_logs[-1].moisture_level
         db.session.commit()
-        if plant.current_moisture_level <= plant.desired_moisture_level:
-            # create reminder
-            return {"message":f"time to remind user. desired moisture: {plant.desired_moisture_level}. current moisture: {plant.current_moisture_level}"}, 200
+        # if plant.current_moisture_level and plant.current_moisture_level <= plant.desired_moisture_level:
+        #     # create reminder
+        #     return {"message":f"time to remind user. desired moisture: {plant.desired_moisture_level}. current moisture: {plant.current_moisture_level}"}, 200
         return [moisture_log.to_dict() for moisture_log in moisture_logs]
         # log number of times watered.
         # calculate average water time
